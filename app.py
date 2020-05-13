9a71e2fe4ac15475e433722afce0d81a1ed23810
Peter Čuřík Jr <91764@stuba.sk>
2020-05-13 23:21:48 +0200

    cache refactor

--- app.py
+++ app.py
@@ -4,18 +4,15 @@
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-#from flask_caching import Cache
 from datetime import datetime
 from werkzeug.contrib.cache import SimpleCache
 
 app = Flask(__name__) #create a flask app
 cors = CORS(app) #enables my website to GET from this server. for more, see enable-cors.org/server_flask.html
 
-#cache = Cache(app, config = {'CACHE_TYPE': 'simple'})  #COFIGURATE cache and create Cache instance
 cache = SimpleCache()
 
 @app.route('/', methods=['POST']) #requests to allow
-#@cache.cached(timeout=0.001)
 def processDataFromApp():
     dic = {}
     incomingData = request.get_json()
@@ -25,45 +22,19 @@ def processDataFromApp():
     passwordItems = incomingData.get('passwordItems')
     bankCardItems = incomingData.get('bankCardItems')
     otherItems = incomingData.get('otherItems')
-    #cacheVal = [sixdigitCode, passwordItems, bankCardItems, otherItems]
-    #print(cache.set('key', 'cacheVal', timeout=50))
-    #dic[deviceID] = [sixdigitCode, dateStr, passwordItems, bankCardItems, otherItems]
     
-    dic[sixdigitCode] = [dateStr, passwordItems, bankCardItems, otherItems]
+    dic[sixdigitCode] = [passwordItems, bankCardItems, otherItems, dateStr]
     cache.set(sixdigitCode,dic,timeout=2*60)
-    #verifyTimeStamps(datetime.now());
-    #return jsonify(dic)
     return cache.get(sixdigitCode)
 
 @app.route('/verify_from_website', methods=['POST'])
 def processDataFromWeb():
-    #verifyTimeStamps(datetime.now());
     incomingData = request.get_json()
     sixdigitTyped = incomingData['sixdigitTyped']
     data = cache.get(sixdigitTyped)
     if data != None:
         return data
     return 'Wrong code'
-    #key = verify(sixdigitTyped)
-    #if key != None:
-     #   return jsonify(dic[key])
-    #return 'Wrong code'
-'''
-def verify(sixdigitTyped):
-    for deviceID in dic.keys():
-        if dic[deviceID][0] == sixdigitTyped:
-            return deviceID
-    return None
-    
-def verifyTimeStamps(currentDate):
-    for item in list(dic.keys()):
-        dateFromCache = datetime.strptime(dic[item][1], '%d/%m/%Y %H:%M:%S')
-        delta = currentDate - dateFromCache
-        minutesDelta = delta.seconds / 60
-        if minutesDelta > 1.99:
-            del dic[item]
-'''
-
 
 if __name__ == '__main__':
     app.run(threaded=False, processes=1)

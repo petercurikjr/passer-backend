a8f0a2399d8317d30e707f2d1cbc4a039e84eed3
Peter Čuřík Jr <91764@stuba.sk>
2020-05-13 21:02:03 +0200

    gunicorn setup

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def processDataFromApp():
     #dic[deviceID] = [sixdigitCode, dateStr, passwordItems, bankCardItems, otherItems]
     
     dic[dateStr] = [sixdigitCode, passwordItems, bankCardItems, otherItems]
-    cache.set(deviceID,dic,timeout=5)
+    cache.set(deviceID,dic,timeout=2*60)
     #verifyTimeStamps(datetime.now());
     #return jsonify(dic)
     return cache.get(deviceID)

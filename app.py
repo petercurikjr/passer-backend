168dbf62119b635d425766bbfd71d3385a949b06
Peter Čuřík Jr <91764@stuba.sk>
2020-05-27 15:25:15 +0200

    duplicates fix

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-#view.py
-#this file handles POST requests from iOS Passer app.
-#after recieving a sixdigit code with uuid of the user's device, Flask builds a dict out of it and writes it to in memory cache in json format for a limited time (2 min)
+#this server script handles POST requests from iOS Passer app and website passer.netlify.app.
+#all data is saved to SimpleCache valid for 2 minutes. no filesystem interaction is used for security and performance reasons.
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
@@ -8,14 +7,16 @@ from datetime import datetime
 from werkzeug.contrib.cache import SimpleCache
 
 app = Flask(__name__) #create a flask app
-cors = CORS(app) #enables javascript to GET from this server. for more, see enable-cors.org/server_flask.html
+cors = CORS(app) #enables javascript to interact with this server. for more, see enable-cors.org/server_flask.html
 
 cache = SimpleCache()
 
+#Passer - six digit code
 @app.route('/sixdigit', methods=['POST']) #requests to allow
 def processSixDigitFromApp():
     dic = {}
     incomingData = request.get_json()
+    
     deviceID = incomingData['deviceID']
     sixdigitCode = incomingData['sixdigitCode']
     dateStr = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
@@ -23,14 +24,25 @@ def processSixDigitFromApp():
     bankCardItems = incomingData.get('bankCardItems')
     otherItems = incomingData.get('otherItems')
     
-    dic[sixdigitCode] = [passwordItems, bankCardItems, otherItems, dateStr]
-    cache.set(sixdigitCode,dic,timeout=2*60) #identifier, value, timeout
-    return 'ok'
+    if cache.has(sixdigitCode):
+        return 409 #conflict - this six digit code is already in the cache
+        
+    if cache.has(deviceID): #check if the same user has already some data in the cache
+        userOldVerifData = cache.get(deviceID)
+        cache.delete(deviceID)
+        cache.delete(userOldVerifData)
+        
+    dic[sixdigitCode] = [passwordItems, bankCardItems, otherItems, dateStr, deviceID]
+    cache.set(deviceID,sixdigitCode,timeout=2*60) #map deviceID to sixdigitCode
+    cache.set(sixdigitCode,dic,timeout=2*60) #map sixdigitCode to passer items data
+    return 'server: ok', 201
 
+#Passer - QR code
 @app.route('/qr', methods=['POST'])
 def processQRFromApp():
     dic = {}
     incomingData = request.get_json()
+    
     sessionID = incomingData['sessionID']
     dateStr = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
     passwordItems = incomingData.get('passwordItems')
@@ -39,8 +51,9 @@ def processQRFromApp():
     
     dic[sessionID] = [passwordItems, bankCardItems, otherItems, dateStr]
     cache.set(sessionID,dic,timeout=2*60)
-    return 'ok'
+    return 'server: ok', 201
 
+#website - six digit code
 @app.route('/verifySixDigitfromwebsite', methods=['POST'])
 def processDataFromWeb():
     incomingData = request.get_json()
@@ -48,9 +61,11 @@ def processDataFromWeb():
     data = cache.get(sixdigitTyped)
     if data != None:
         cache.delete(sixdigitTyped)
+        cache.delete(data[-1]) #last element of data list (deviceID)
         return data
     return 'Wrong code'
     
+#website - QR code
 @app.route('/verifyQRfromwebsite', methods=['POST'])
 def processSessionIDFromWeb():
     incomingData = request.get_json()
